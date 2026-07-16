fb71a6f77009cfe568a7aeddeffc50901fd471f9
devarshigupta <devarshigupta457@gmail.com>
2026-07-16 13:12:12 +0530

    Added Latest chnages

--- kt-session-fe/src/environments/environment.ts
+++ kt-session-fe/src/environments/environment.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://kt-session-app-project-production.up.railway.app'
+  // apiUrl: 'https://kt-session-app-project-production.up.railway.app'
+    apiUrl: 'http://localhost:8080'
 };
+
+
